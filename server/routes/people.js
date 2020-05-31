d5b6d8841aada3c5c20d89116efc506660087a77
Daniel Collings <Daniel.Collings@medicorsoftware.com>
2020-05-31 22:50:53 +0100

    update to server

--- server/routes/people.js
+++ server/routes/people.js
@@ -2,25 +2,27 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-//https://www.latlong.net/place/london-the-uk-14153.html London coords
-//http://jsfiddle.net/edgren/gAHJB/ Calculating distance between lat/long coords
+//https://www.latlong.net/place/london-the-uk-14153.html London coords.
+//https://www.movable-type.co.uk/scripts/latlong.html Calculating distance between lat/long coords (Haversine formula).
         
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
+function GetDistanceBetweenTwoCoordinates(lat1,lon1,lat2,lon2) {
+  var radius = 6371;
+
+  var dLat = ConvertToRadian(lat2-lat1); 
+  var dLon = ConvertToRadian(lon2-lon1); 
+
   var a = 
     Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
+    Math.cos(ConvertToRadian(lat1)) * Math.cos(ConvertToRadian(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
+  var d = radius * c;
   return d;
 }
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
+function ConvertToRadian(degrees) {
+  return degrees * (Math.PI/180)
 }
 
 router.get('/', function(req, res, next) {
@@ -33,12 +35,9 @@ router.get('/', function(req, res, next) {
       var data = JSON.parse(body);
 
       var arr =  data.filter(function(person) {
-        var distance = getDistanceFromLatLonInKm(51.509865, -0.118092, person.latitude, person.longitude);
-        console.log(distance)
+        var distance = GetDistanceBetweenTwoCoordinates(51.509865, -0.118092, person.latitude, person.longitude);
         return distance <= 80.4672;
       });
-
-      console.dir(arr);
 
       res.send(arr);
   });
